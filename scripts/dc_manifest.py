33fcac1c766e5e1de94afca77800c19cdf508774
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-04-26 22:56:00 +1200

    list song samples; improve cmdline & output

--- scripts/dc_manifest.py
+++ scripts/dc_manifest.py
@@ -1,47 +1,61 @@
 #!dc_manifest
 """
-list deluge_card contents of folder specified
+list deluge_card contents of folder(s) specified
 """
 import argparse
 from deluge_card import DelugeCardFS
 from deluge_card.deluge_card import InvalidDelugeCard, list_deluge_fs
 
-def list_songs(card):
-	cs = card.songs()
-	print(f'Deluge filesystem has {len(cs)} songs')
+def list_songs(card, args):
+	cs = list(card.songs())
+	if args.summary | args.verbose:
+		print(f'Deluge filesystem {card} has {len(cs)} songs')	
+	if args.summary:
+		return	
 	for s in cs:
-		print(f'  song {s} key {s.scale()}')
+		print(f'  song: {s} key {s.scale()}')			
 
-def list_samples(card):
-	cs = card.samples()
-	print(f'Deluge filesystem has {len(cs)} samples')
+def list_samples(card, args):
+	cs = list(card.samples())
+	if args.summary | args.verbose:	
+		print(f'Deluge filesystem {card} has {len(cs)} samples')
+	if args.summary:
+		return		
+	for sa in cs:
+		print(f'  samp: {sa.path()}')
+
+def list_song_samples(card, args):
+	cs = list(card.songs())
 	for s in cs:
-		print(f'  sample: {s}')
-
-
+		if args.summary | args.verbose:
+			print(f'  song: {s} has {len(list(s.samples()))} samples')
+		if args.summary:
+			continue
+		for sa in s.samples():
+			print(f'  samp: {sa.path()} settings: {sa.settings()}')
 
 if __name__ == '__main__':
-	print('dc_manifest')
+	#print('dc_manifest')
 	parser = argparse.ArgumentParser(description='list deluge_card contents')
 	parser.add_argument('folder', type=str, nargs='+',
 	                    help='one or more folder to check')
-	parser.add_argument('-l', '--list', nargs='+',
-	                    help='include listing of s=songs, S=samples, K=Kit, I=instrument')
+	parser.add_argument('-l', '--list',
+	                    help='one of of s=songs, a=samples, S=song_samples (future: K=Kit, I=instrument)')
+	parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")	
+	parser.add_argument("-s", "--summary", help="summarise output",
+                    action="store_true")	
 
 	args = parser.parse_args()
-	print(args.folder)
 	
 	for fname in args.folder:
 		card_imgs = list_deluge_fs(fname)
 		if len(card_imgs):
 			for c in card_imgs:
 				print(f'Deluge filesystem at {c.card_root()} mounted: {c.is_mounted()}')
-				opts = [x for x in ''.join(args.list)]
-				print(opts)
-				if 's' in opts:
-					print('Songs')
-					list_songs(c)
-
-				if 'S' in opts:
-					print('Samples')
-					list_samples(c)
+				if args.list == 's':
+					list_songs(c, args)
+				if args.list ==  'a':
+					list_samples(c, args)
+				if args.list == 'S':
+					list_song_samples(c, args)
